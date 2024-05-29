25cff45031909b3f852d0a0eb82e9ab4e904311b
Lucas Lion <lucaslion7@gmail.com>
2024-05-29 19:30:12 +0200

    textField pseudo, connection button, homepage

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:projetp/pages/home_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Pseudo',
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20), // This is the border radius
+              ),
+              padding: EdgeInsets.all(10), // This is the padding
+              minimumSize: Size(150, 50), // This is the minimum size
+            ),
+            child: const Text(
+              'Connexion',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
